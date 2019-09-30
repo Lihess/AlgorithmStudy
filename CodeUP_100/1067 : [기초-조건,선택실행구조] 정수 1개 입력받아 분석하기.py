cb460d19f8ca591da4a8159c937a6d66e4e8811b
LeeEunbi <50014287+Lihess@users.noreply.github.com>
2019-10-01 00:11:06 +0900

    Create 1067 : [기초-조건,선택실행구조] 정수 1개 입력받아 분석하기.py

---
+++ CodeUP_100/1067 : [기초-조건,선택실행구조] 정수 1개 입력받아 분석하기.py
@@ -0,0 +1,14 @@
+a = input()
+a = int(a)
+
+if a < 0 : 
+    print("minus")
+else : 
+    print ("plus")
+
+if (a % 2 == 0) : 
+    print("even")
+else:
+    print("odd")
+    
+# 참고로 "else if"는 elif!
