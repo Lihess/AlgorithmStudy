f6b9bf55bf141e59e7bebb2424ce62a3021ebb3a
이은비 <she6569@gmail.com>
2019-10-06 19:41:48 +0900

    Greedy : 11399 solved

---
+++ Greedy/11399_ATM.py
@@ -0,0 +1,13 @@
+sum = 0 # 시간의 합
+n = int(input()) # n명 입력
+p = list(map(int, input().split())) # 실제 입려될 작업시간
+
+if len(p) == n : 
+    p.sort() # n명이 입력된 경우, 오름차순으로 정렬한다.
+
+for i in range(0, n) :
+    for j in range(0, i + 1) : 
+        sum += p[j]
+
+print(sum)
+
