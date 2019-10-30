012c675f24878522d3d7891973ccbeb572f78160
이은비 <she6569@gmail.com>
2019-10-31 00:03:00 +0900

    1781 : 시간초과

--- Greedy/[백준]1781_컵라면.py
+++ Greedy/[백준]1781_컵라면.py
@@ -3,7 +3,7 @@ from queue import PriorityQueue
 n = int(input())
 num = []
 out = []
-sum = 0
+sum_ = 0
 
 for i in range(0, n) : 
     num.append(list(map(int, input().split())))
@@ -13,6 +13,23 @@ num.sort(key = lambda num : (num[0], -num[1]))
 que = PriorityQueue()
 
 for i in num : 
-    que.put((i[0], -i[1]))
+    que.put((-i[1], i[0]))
 
+while not que.empty() :
+    x = que.get()
 
+    j_n = int(out.count(x[1]))
+    j = 0
+    while j <= j_n :
+        if ((x[1] - j) > 0) :
+            overlapping = out.count(x[1] - j)
+        else : break
+
+        if overlapping == 0 :
+            out.append(x[1] - j)
+            sum_ += abs(x[0])
+            break
+        else :  
+            j += 1
+        
+print(sum_)
