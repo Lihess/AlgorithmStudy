9be33449886ae61729e23bbe72b7e8a212f582fa
이은비 <she6569@gmail.com>
2019-11-26 00:38:28 +0900

    1260 : 순서가 이상해

--- BFS, DFS/[백준]1260_DFS와 BFS.py
+++ BFS, DFS/[백준]1260_DFS와 BFS.py
@@ -1,5 +1,35 @@
 N, M, start = map(int, input().split())
-graph = [[0] * (N + 1) for in range(N+1)]
+graph = [[] for _ in range(N + 1)]
 
-for i in range(N+1) : 
-    link = list(map(int, input().split))
+for i in range(M) : 
+    link = list(map(int, input().split()))
+    graph[link[0]].append(link[1])
+    graph[link[1]].append(link[0])
+
+def dfs(graph, start) : 
+    visited = []
+    queue = [start]
+
+    while queue : 
+        node = queue.pop(0)
+
+        if node not in visited : 
+            visited.append(node)
+            queue += set(graph[node]) - set(visited)
+    return visited
+
+def bfs(graph, start) : 
+    visited = []
+    stack = [start]
+
+    while stack : 
+        print(stack)
+        node = stack.pop()
+
+        if node not in visited : 
+            visited.append(node)
+            stack += (set(graph[node]) - set(visited))
+    return visited
+
+print(*dfs(graph, start))     
+print(*bfs(graph, start))            
