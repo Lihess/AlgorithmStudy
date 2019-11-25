b19a4b214a3481cbd2cf5cef835f9e3eebf26674
이은비 <she6569@gmail.com>
2019-11-26 00:52:01 +0900

    1260 : solved!

--- BFS, DFS/[백준]1260_DFS와 BFS.py
+++ BFS, DFS/[백준]1260_DFS와 BFS.py
@@ -5,6 +5,9 @@ for i in range(M) :
     link = list(map(int, input().split()))
     graph[link[0]].append(link[1])
     graph[link[1]].append(link[0])
+    
+for i in range(0, N + 1) : 
+    graph[i].sort()
 
 def dfs(graph, start) : 
     visited = []
@@ -14,8 +17,10 @@ def dfs(graph, start) :
         node = queue.pop(0)
 
         if node not in visited : 
-            visited.append(node)
-            queue += set(graph[node]) - set(visited)
+            visited.append(node) 
+            for n in range(0,len(graph[node])) : 
+                if graph[node][n] not in visited : 
+                    queue.append(graph[node][n])
     return visited
 
 def bfs(graph, start) : 
@@ -27,8 +32,10 @@ def bfs(graph, start) :
         node = stack.pop()
 
         if node not in visited : 
-            visited.append(node)
-            stack += (set(graph[node]) - set(visited))
+            visited.append(node) 
+            for n in range(len(graph[node]) - 1, -1, -1) : 
+                if graph[node][n] not in visited : 
+                    stack.append(graph[node][n] )
     return visited
 
 print(*dfs(graph, start))     
